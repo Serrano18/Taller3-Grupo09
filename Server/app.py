fd763cea0b5791869a59cc730f0ea911c3cc9325
Serrano18 <3993532670115@ingenieria.usac.edu.gt>
2023-08-30 08:38:38 -0600

    API

--- Server/app.py
+++ Server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -15,6 +15,7 @@ mysql = MySQL(app)
 def index():
     return jsonify({"message": "Bienvenido a mi aplicación"})
 
+
 @app.route('/users', methods=['GET'])
 def get_users():
     cur = mysql.connection.cursor()
@@ -23,13 +24,19 @@ def get_users():
     cur.close()
     return jsonify(data)
 
-@app.route('/users/<int:user_id>', methods=['GET'])
-def get_user(user_id):
+
+@app.route('/users/<string:user_phone>', methods=['GET'])
+def get_user_by_phone(user_phone):
     cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM users WHERE id = %s", (user_id,))
+    cur.execute("SELECT * FROM users WHERE phone = %s", (user_phone,))
     data = cur.fetchone()
     cur.close()
-    return jsonify(data)
+
+    if data:
+        return jsonify(data)
+    else:
+        return jsonify({"message": "User not found"}), 404
+
 
 @app.route('/users', methods=['POST'])
 def add_user():
@@ -38,39 +45,41 @@ def add_user():
     last_name = data['last_name']
     phone = data['phone']
     email = data['email']
-    
+
     cur = mysql.connection.cursor()
     cur.execute("INSERT INTO users (name, last_name, phone, email) VALUES (%s, %s, %s, %s)",
                 (name, last_name, phone, email))
     mysql.connection.commit()
     cur.close()
-    
+
     return jsonify({"message": "User added successfully"})
 
-@app.route('/users/<int:user_id>', methods=['PUT'])
-def update_user(user_id):
+
+@app.route('/users/<string:user_phone>', methods=['PUT'])
+def update_user_by_phone(user_phone):
     data = request.json
     name = data['name']
     last_name = data['last_name']
-    phone = data['phone']
     email = data['email']
-    
+
     cur = mysql.connection.cursor()
-    cur.execute("UPDATE users SET name = %s, last_name = %s, phone = %s, email = %s WHERE id = %s",
-                (name, last_name, phone, email, user_id))
+    cur.execute("UPDATE users SET name = %s, last_name = %s, email = %s WHERE phone = %s",
+                (name, last_name, email, user_phone))
     mysql.connection.commit()
     cur.close()
-    
+
     return jsonify({"message": "User updated successfully"})
 
-@app.route('/users/<int:user_id>', methods=['DELETE'])
-def delete_user(user_id):
+
+@app.route('/users/<string:user_phone>', methods=['DELETE'])
+def delete_user_by_phone(user_phone):
     cur = mysql.connection.cursor()
-    cur.execute("DELETE FROM users WHERE id = %s", (user_id,))
+    cur.execute("DELETE FROM users WHERE phone = %s", (user_phone,))
     mysql.connection.commit()
     cur.close()
-    
+
     return jsonify({"message": "User deleted successfully"})
+
 
 def create_user_table():
     cur = mysql.connection.cursor()
@@ -86,8 +95,9 @@ def create_user_table():
     mysql.connection.commit()
     cur.close()
 
+
 if __name__ == '__main__':
     with app.app_context():
         create_user_table()
     app.run(host='0.0.0.0', port=5000)
-    
+
